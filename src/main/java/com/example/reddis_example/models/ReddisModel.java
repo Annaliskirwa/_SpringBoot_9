3c2a1de729b0724deac9f57167e4ea3c5babc1db
Annaliskirwa <annaliskirwa@gmail.com>
2023-01-19 11:23:13 +0200

    db models

--- src/main/java/com/example/reddis_example/models/ReddisModel.java
+++ src/main/java/com/example/reddis_example/models/ReddisModel.java
@@ -2,6 +2,23 @@ package com.example.reddis_example.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BPR_NOTIFICATION")
 @Data
 public class ReddisModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "MESSAGE_ID")
+    private String messageID;
+    @Column(name="TRANSACTION_REFERENCE")
+    private String transactionReference;
+    @Column(name = "NOTIFICATION_TYPE")
+    private String noficationType;
+    @Column(name = "MESSAGE")
+    private String message;
 }
