7c871b26dc5d64c9d49c2ab9dc2cae175b42cf5c
Annaliskirwa <annaliskirwa@gmail.com>
2023-01-19 19:02:47 +0200

    sequence gen for data id

--- src/main/java/com/example/reddis_example/models/RedisModel.java
+++ src/main/java/com/example/reddis_example/models/RedisModel.java
@@ -1,14 +1,22 @@
 package com.example.reddis_example.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
 import java.io.Serializable;
 
 @Data
 @RedisHash("Notification")
-public class RedisModel{
-    private int id;
+public class RedisModel implements Serializable {
+    private static final long serialVersionUID = 7156526077883281623L;
+    @Id
+    @SequenceGenerator(name = "NOTIF_GEN", sequenceName = "NOTIF", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "NOTIF_GEN")
+    private Long id;
     private String messageID;
     private String transactionReference;
     private String notificationType;
