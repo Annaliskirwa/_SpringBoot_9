a5c995dd96240427012b882712b79392025ecbb8
Annaliskirwa <annaliskirwa@gmail.com>
2023-01-19 12:18:05 +0200

    jeddis clients dependencies fix

--- src/main/java/com/example/reddis_example/config/ReddisConfig.java
+++ src/main/java/com/example/reddis_example/config/ReddisConfig.java
@@ -2,6 +2,7 @@ package com.example.reddis_example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -9,10 +10,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class ReddisConfig {
     @Bean
     JedisConnectionFactory jedisConnectionFactory(){
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("localhost");
-        jedisConnectionFactory.setPort(6379);
-        return new JedisConnectionFactory();
+//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+//        jedisConnectionFactory.setHostName("localhost");
+//        jedisConnectionFactory.setPort(6379);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
