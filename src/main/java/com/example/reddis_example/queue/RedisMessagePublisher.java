12a914963c1ea79811b1a18ce874712cfe0f05de
Annaliskirwa <annaliskirwa@gmail.com>
2023-01-20 12:36:26 +0200

    redis publish message on post request

--- src/main/java/com/example/reddis_example/queue/RedisMessagePublisher.java
+++ src/main/java/com/example/reddis_example/queue/RedisMessagePublisher.java
@@ -3,7 +3,9 @@ package com.example.reddis_example.queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RedisMessagePublisher implements MessagePublisher{
 
     @Autowired
--- src/main/java/com/example/reddis_example/service/implementation/ReddisServiceImpl.java
+++ src/main/java/com/example/reddis_example/service/implementation/ReddisServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.reddis_example.service.implementation;
 
 import com.example.reddis_example.models.RedisModel;
+import com.example.reddis_example.queue.RedisMessagePublisher;
 import com.example.reddis_example.repository.ReddisRepository;
 import com.example.reddis_example.service.ReddisService;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,18 @@ import java.util.List;
 public class ReddisServiceImpl implements ReddisService {
     @Autowired
     ReddisRepository reddisRepository;
+
+    @Autowired
+    RedisMessagePublisher redisMessagePublisher;
     @Override
     public ResponseEntity<?> storeData(RedisModel redisModel) {
         reddisRepository.save(redisModel);
         log.info("The data has been saved in the database {}", redisModel);
+
+//        Reddis sending and receiving the messages
+        Gson gson = new Gson();
+        String message = gson.toJson(redisModel);
+        redisMessagePublisher.publish(message);
         return ResponseEntity.ok(redisModel);
     }
 
